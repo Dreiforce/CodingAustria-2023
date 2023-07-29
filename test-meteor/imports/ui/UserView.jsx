2e7e0506edc2a27322def55ddb15290337739c8e
Simon Lehner-Dittenberger <simon.lehner-dittenberger@cloudflight.io>
2023-07-29 22:17:54 +0200

    test with different push/sync/state

--- test-meteor/imports/ui/UserView.jsx
+++ test-meteor/imports/ui/UserView.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useFind, useSubscribe } from 'meteor/react-meteor-data';
 import { Router } from 'meteor/iron:router'
 import { LinksCollection, UserStateCollection } from '../api/links';
 
@@ -6,7 +7,11 @@ import { Meteor } from 'meteor/meteor';
 
 export const UserView = ({ teamName }) => {
 
+
+
   console.log("init user view again")
+  
+ // initState = UserStateCollection.findOne({teamName: teamName})
 
   const [state, setState] = useState({
     teamName: teamName,
@@ -43,7 +48,7 @@ const toggle = (value) => {
 
   return (
     <div>
-      <h1>i am {teamName} </h1>
+      <h1>i am {state.teamName} </h1>
       <button  onClick={pressSmtn("stateTestA")}>press A={state["stateTestA"]}</button>
       <button  onClick={pressSmtn("stateTestB")}>press B={state["stateTestB"]}</button>
     </div>
