c2c884e36e576cd8cc996fbf6c694a8e42ba19ae
Simon Lehner-Dittenberger <simon.lehner-dittenberger@cloudflight.io>
2023-07-29 23:56:58 +0200

    Add admin and web view

--- demoreactapp/client/src/components/ClientView.js
+++ demoreactapp/client/src/components/ClientView.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import MapView from './MapView';
 
 function ClientView() {
     return (
         <div>
             ClientView
+            <MapView></MapView>
         </div>
     );
 }
