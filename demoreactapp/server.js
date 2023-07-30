23d9741675252fab8f0ac22ef0388ec28dfbaa49
Simon Lehner-Dittenberger <simon.lehner-dittenberger@cloudflight.io>
2023-07-30 04:30:32 +0200

    improve server with multi id support

--- demoreactapp/server.js
+++ demoreactapp/server.js
@@ -12,33 +12,40 @@ app.get('/express_backend', (req, res) => { //Line 9
 }); //Line 11
 
 // In-memory message queue
-const messageQueue = [];
+const messageQueue = {};
 
 // Add a message to the queue
-function enqueueMessage(message) {
-  messageQueue.push(message);
+function enqueueMessage(id, message) {
+  if (messageQueue[id] == undefined) {
+    messageQueue[id] = []
+  }
+  messageQueue[id].push(message);
 }
 
 // Get the next message from the queue
-function dequeueMessage() {
-  return messageQueue.shift();
+function dequeueMessage(id) {
+  if(messageQueue[id] === undefined)
+  return undefined;
+  return messageQueue[id].shift();
 }
 
 // Route to add messages to the queue
 app.post('/enqueue/:id', (req, res) => {
   const id = req.params.id;
   const message = req.body.message;
-  console.log(`Enqueue message: ${message}`)
-  enqueueMessage(message);
+  console.log(`Enqueue message for id ${id}: ${message}`)
+  enqueueMessage(id, message);
   res.status(200).json({ message: 'Message enqueued successfully' });
 });
 
 // Route to retrieve messages from the queue
 app.get('/dequeue/:id', (req, res) => {
   const id = req.params.id;
-  const message = dequeueMessage();
+  const message = dequeueMessage(id);
   if (message) {
     console.log(`Dequeue message: ${message}`)
-    res.json({ message });
+    res.json({ empty: false,  message: message });
+  } else {
+    res.json({ empty: true, message: ""})
   }
 });
